ffda6719e18bef2621b24bc232f11b3d8d571a69
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-23 11:29:23 -0700

    created intro, added README

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#tracker").submit(function(event) {
+    var Q1 = parseInt($("input#side1").val());
+    var Q2 = parseInt($("input#side2").val());
+    var Q3 = parseInt($("input#side3").val());
+    var Q4 = parseInt($("input#side1").val());
+    var Q5 = parseInt($("input#side2").val());
+
+  });
+});
