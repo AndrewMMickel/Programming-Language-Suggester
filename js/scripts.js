c678033a08f84197bd8379bc5a60bc4be91f6048
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-29 11:10:02 -0700

    fixing functions to accept result answer

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,12 @@ $(document).ready(function() {
     var Q4 = $("select#Q4").val();
     var Q5 = $("select#Q5").val();
 
-        if (Q1 === 'Yes' && Q3 === '0' && Q4 === 'Lenovo') {
-          var coding = "javascript"
+        if (Q1 === 'Yes' && Q2 === 'software programming' && Q3 === '0-1' && Q4 === 'Lenovo' && Q5 === 'Yes') {
+            var coding = "javascript";
         }
+        //else if (Q1 === 'No' && Q2 === 'web design' && Q3 === '0-1' && Q4 === 'Lenovo' && Q5 === 'Yes') {
+            //var coding = "Ruby"
+        //}
 
         $("#coding").text(coding);
         $('#result').show()
